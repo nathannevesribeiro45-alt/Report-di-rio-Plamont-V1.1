ca954516b34826a12cb69ff4911ae06d4751c218
Jonathan Ribeiro <nathannevesribeiro45@gmail.com>
2026-09-11 08:33:56 -0300

    att

--- js/modules/dashboard.js
+++ js/modules/dashboard.js
@@ -13,11 +13,11 @@ const dashboardStatus = {
         "Nenhuma ocorrência crítica registrada neste turno."
     ],
 
-    turno: "Dia B",
+    turno: "Dia A",
 
     horario: "07:00 às 19:00",
 
-    atualizacao: "10/09/2026 às 07:00"
+    atualizacao: "11/09/2026 às 07:00"
 
 };
 
